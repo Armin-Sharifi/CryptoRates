884cab58cac743774ea6a917f68344f14100edb4
Armin Sharifi <arminsharifi@outlook.com>
2025-04-25 18:16:32 +0330

    Enhance AddApplicationServices with Swagger and HttpClient
    Updated the `AddApplicationServices` method in the `ServiceExtensions` class to include Swagger API Explorer, Swagger generation, and HTTP client service registrations. These changes improve API documentation and interaction capabilities.

--- CryptoRates.UI.API/Extensions/ServiceExtensions.cs
+++ CryptoRates.UI.API/Extensions/ServiceExtensions.cs
@@ -4,8 +4,11 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        // Add Swagger Api Explorer
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
+        services.AddHttpClient(); 
 
         return services;
     }
